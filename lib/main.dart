57431bc798b5ac3c66ed3daad06e6926a57da886
lone-wolf <adeyanju.akorede1@gmail.com>
2021-01-22 17:28:23 +0100

    ** Bottom Navigation UI Built ** Header UI Built ** Search Widget Ui Built

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app_ui/screens/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: HomePage(),
     );
   }
 }
